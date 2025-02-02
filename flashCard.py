91b4bad41407818685ca9befec31f2d88327a1c5
Leo Phillips <62564431+Mr-L-Web@users.noreply.github.com>
2025-02-02 15:51:50 +0000

    flashCard v.1.1
    Fixed the text glitch when marking an answer. The correct and incorrect strings don't overlay
    Added a menu button to go back to the home screen

--- flashCard.py
+++ flashCard.py
@@ -12,30 +12,20 @@ capital_list = {'Capital of France?': 'Paris',
                 'Capital of Austrailia?': 'Canberra'} #Dictionary for the Capital city subject
 
 
-
-
-
-selectLbl = Label(root, text = "Select your subject") #Label for the home screen
-selectLbl.grid(column = 2, row = 0)
-
-
-
-answer = Entry(root, width = 10) #Text box for answers
-
-cap = key = random.choice(list(capital_list.keys())) #The app picks a capital key from the dictionary at random
-
-   
+    
 def capital():
-    capCity = Label(root, text = cap)
-    capCity.grid(column = 1, row = 3)
-    selectLbl.grid_remove()
+    capCities.grid(column = 0, row = 0)
+    global cap
+    global capCity
+    cap = random.choice(list(capital_list.keys())) #The app picks a capital key from the dictionary at random
+    capCity = Label(capCities, text = cap)
+    capCity.grid(column = 1, row = 1)
+    home.grid_remove()
     sub_Answer.grid(column = 3, row = 4)
-    capitalLbl.grid_remove()
     answer.grid(column = 1, row = 4)
 
+
 def submit():
-    markIncorrect = Label(root, text = "incorrect")
-    markCorrect = Label(root, text = "correct")
     if answer.get().lower() == capital_list.get(cap).lower():
         markIncorrect.grid_remove()
         markCorrect.grid(column = 2, row = 5)
@@ -43,18 +33,43 @@ def submit():
         markCorrect.grid_remove()
         markIncorrect.grid(column = 2, row = 5)
         
-    
+def menu():
+    capCities.grid_remove()
+    capCity.grid_remove()
+    home.grid()
 
-sub_Answer = Button(root, text = "Submit Answer", command = submit)
 
 
-capitalLbl = Button(root, text = "Capital Cities", command = capital)
+
+home = Frame(root)
+selectLbl = Label(home, text = "Select your subject") #Label for the home screen
+selectLbl.grid(column = 2, row = 0)
+capitalLbl = Button(home, text = "Capital Cities", command = capital)
 capitalLbl.grid(column = 1, row = 1)
 
 
 
 
 
+
+    
+    
+
+capCities = Frame(root)
+answer = Entry(capCities, width = 10) #Text box for answers
+markIncorrect = Label(capCities, text = "Incorrect")
+markCorrect = Label(capCities, text = "Correct")
+sub_Answer = Button(capCities, text = "Submit Answer", command = submit)
+
+
+
+
+
+
+menuButt = Button(root, text = "Menu", command = menu)
+menuButt.grid(column = 0, row = 6)
+
+home.grid(column = 0, row = 0)
 
 
 
