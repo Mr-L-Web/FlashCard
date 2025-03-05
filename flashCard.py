1c37ad33f9f887c51b0d56ad05a75e5d1ad6c1c3
Leo Phillips <62564431+Mr-L-Web@users.noreply.github.com>
2025-03-05 20:28:34 +0000

    Updated with new section
    Addeded more counties to the country list and added another section for times tables.

--- flashCard.py
+++ flashCard.py
@@ -9,67 +9,126 @@ root.geometry('350x200')#Sets the geometry for the window
 
 capital_list = {'Capital of France?': 'Paris',
                 'Capital of Spain?' : 'Madrid',
-                'Capital of Austrailia?': 'Canberra'} #Dictionary for the Capital city subject
+                'Capital of Austrailia?': 'Canberra',
+                'Capital of USA?': 'Washington DC',
+                'Capital of UK?': 'London',
+                'Capital of Italy?': 'Rome',
+                'Capital of Japan?': 'Tokyo',
+                'Capital of New Zealand?': 'Wellington',
+                'Capital of Brazil?': 'Brasilia',
+                'Capital of Canada?': 'Ottawa'} #Dictionary for the Capital city subject
+
+
+times_table_list = {'2 x 4 = ?' : '8',
+                    '5 x 6 = ?' : '30',
+                    '8 x 3 = ?' : '24',
+                    '7 x 9 = ?' : '63',
+                    '12 x 13 =?': '156',
+                    '11 x 6 = ?' : '66'}
+
+home = Frame(root)
+selectLbl = Label(home, text = "Select your subject") #Label for the home screen
+selectLbl.grid(column = 2, row = 0)
+
+
+
+capCities = Frame(root)
+timTables= Frame(root)
+answerC = Entry(capCities, width = 10) #Text box for answers
+answerTT = Entry(timTables, width = 10)
+markIncorrectC = Label(capCities, text = "Incorrect")
+markCorrectC = Label(capCities, text = "Correct")
+markIncorrectTT = Label(timTables, text = "Incorrect")
+markCorrectTT = Label(timTables, text = "Correct")
+
+
+
+
+
+
+
+
+
+
 
 
     
 def capital():
     capCities.grid(column = 0, row = 0)
+    menuButtC.grid(column = 0, row = 6)
     global cap
-    global capCity
     cap = random.choice(list(capital_list.keys())) #The app picks a capital key from the dictionary at random
+    global capCity
     capCity = Label(capCities, text = cap)
     capCity.grid(column = 1, row = 1)
     home.grid_remove()
-    sub_Answer.grid(column = 3, row = 4)
-    answer.grid(column = 1, row = 4)
+    sub_AnswerC.grid(column = 3, row = 4)
+    answerC.grid(column = 1, row = 4)
 
 
-def submit():
-    if answer.get().lower() == capital_list.get(cap).lower():
-        markIncorrect.grid_remove()
-        markCorrect.grid(column = 2, row = 5)
+def timesTable():
+    global tim
+    global table
+    timTables.grid(column = 0, row = 0)
+    menuButtTT.grid(column = 0, row = 6)
+    tim = random.choice(list(times_table_list.keys())) #The app picks a times table key from the dictionary at random
+    table = Label(timTables, text = tim)
+    table.grid(column = 1, row = 1)
+    home.grid_remove()
+    sub_AnswerTT.grid(column = 3, row = 4)
+    answerTT.grid(column = 1, row = 4)
+
+
+
+def submitC():
+    if answerC.get().lower() == capital_list.get(cap).lower():
+        markIncorrectC.grid_remove()
+        markCorrectC.grid(column = 2, row = 5)
     else:
-        markCorrect.grid_remove()
-        markIncorrect.grid(column = 2, row = 5)
+        markCorrectC.grid_remove()
+        markIncorrectC.grid(column = 2, row = 5)
+
+
+def submitTT():
+    if answerTT.get().lower() == times_table_list.get(tim):
+        markIncorrectTT.grid_remove()
+        markCorrectTT.grid(column = 2, row = 5)
+    else:
+        markCorrectTT.grid_remove()
+        markIncorrectTT.grid(column = 2, row = 5)
         
-def menu():
+def menuC():
     capCities.grid_remove()
     capCity.grid_remove()
+    menuButtC.grid_remove()
+    home.grid()
+
+def menuTT():
+    timTables.grid_remove()
+    table.grid_remove()
+    menuButtTT.grid_remove()
     home.grid()
 
 
 
-
-home = Frame(root)
-selectLbl = Label(home, text = "Select your subject") #Label for the home screen
-selectLbl.grid(column = 2, row = 0)
 capitalLbl = Button(home, text = "Capital Cities", command = capital)
 capitalLbl.grid(column = 1, row = 1)
+timeLbl = Button(home, text = "Times Tables", command = timesTable)
+timeLbl.grid(column = 2 , row = 1)
+
+sub_AnswerC = Button(capCities, text = "Submit Answer", command = submitC)
+sub_AnswerTT = Button(timTables, text = "Submit Answer", command = submitTT)
 
 
-
-
-
-
-    
-    
-
-capCities = Frame(root)
-answer = Entry(capCities, width = 10) #Text box for answers
-markIncorrect = Label(capCities, text = "Incorrect")
-markCorrect = Label(capCities, text = "Correct")
-sub_Answer = Button(capCities, text = "Submit Answer", command = submit)
-
-
-
-
-
-
-menuButt = Button(root, text = "Menu", command = menu)
-menuButt.grid(column = 0, row = 6)
+menuButtC = Button(root, text = "Menu", command = menuC)
+menuButtTT = Button(root, text = "Menu", command = menuTT)
 
 home.grid(column = 0, row = 0)
+
+
+    
+    
+
 
 
 
